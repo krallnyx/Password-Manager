790b52bf88d626849abc40886d2d9e7d9373c950
Arnaud Jeanroch <arnaud@jeanroch.fr>
2021-02-27 18:12:30 +0000

    #2 inactive fields added

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
 from tkinter import *
 
+FONT_NAME = "Arial"
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    pass
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def add_password():
+    pass
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(column=1, row=0, pady=2)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1, pady=2)
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2, sticky="EW", pady=2)
+
+login_label = Label(text="Email/Username:")
+login_label.grid(column=0, row=2, pady=2)
+login_entry = Entry(width=35)
+login_entry.grid(column=1, row=2, columnspan=2, sticky="EW", pady=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3, pady=2)
+password_entry = Entry()
+password_entry.grid(column=1, row=3, sticky="EW", pady=2)
+generate_button = Button(text="Generate Password", command=generate_password)
+generate_button.grid(column=2, row=3, sticky="EW", pady=2)
+
+Add_button = Button(text="Add", width=35, command=add_password)
+Add_button.grid(column=1, row=4, columnspan=2, sticky="EW", pady=2)
 
 window.mainloop()
