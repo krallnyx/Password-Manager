01808191defe47c54dd773c40de0add841748b42
Arnaud Jeanroch <arnaud@jeanroch.fr>
2021-02-28 12:26:38 +0000

    #4 random password generation and dialog box for field validation

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from tkinter import *
-
-FONT_NAME = "Arial"
+from tkinter import messagebox
+import random
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    pass
+    password_entry.delete(0, END)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [random.choice(letters) for _ in range(random.randint(8, 10))]
+    password_numbers = [random.choice(numbers) for _ in range(random.randint(2, 4))]
+    password_symbols = [random.choice(symbols) for _ in range(random.randint(2, 4))]
+    password_list = password_letters + password_numbers + password_symbols
+    random.shuffle(password_list)
+
+    password_entry.insert(0, "".join(password_list))
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -13,11 +26,16 @@ def save():
     website = website_entry.get()
     login = login_entry.get()
     password = password_entry.get()
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website} | {login} | {password}\n")
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
 
+    if len(website) == 0 or len(password) == 0 or len(login) == 0:
+        messagebox.showerror(title="Oops", message="Please make sure to fill every fields")
+    else:
+
+        with open("data.txt", "a") as data_file:
+            data_file.write(f"{website} | {login} | {password}\n")
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+        messagebox.showinfo(title="Success", message=f"The details for {website} have been saved.")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
